b7498e9be655152a08812a72ab0ad79c6a85be7b
Daniel Lazar <lazardani0112@gmail.com>
2021-12-24 10:27:49 +0100

    feat(layout): Change layout animation

---
+++ hooks/useSlaceUp.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+
+const useScaleUp = (element: string, duration: number) => {
+  useEffect(() => {
+    gsap.fromTo(
+      element,
+      {
+        opacity: 0,
+        scale: 0.5,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        duration,
+      }
+    );
+  });
+};
+
+export default useScaleUp;
