00a75d4f93a694c9f34d458a0036eb477f430d94
Daniel Lazar <lazardani0112@gmail.com>
2022-04-11 17:54:16 +0200

    feat(gifts): add gift button

--- pages/no-gift.tsx
+++ pages/no-gift.tsx
@@ -1,15 +1,13 @@
-import {NextPage} from "next";
+import { NextPage } from 'next';
 import { SelfClosingComponent } from '../components/code';
-import Layout from "../components/layout";
+import Layout from '../components/layout';
 
 const NoGift: NextPage = () => {
   return (
     <Layout>
       <SelfClosingComponent
         name="Info"
-        props={[
-          {name: 'message', value: 'Out of gifts, sorry! :('},
-        ]}
+        props={[{ name: 'message', value: 'Out of gifts, sorry! :(' }]}
       />
     </Layout>
   );
